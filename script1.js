72bb8f1b48d370b627fab5aee2e94942d887a394
hennan1 <hannanbutt0267@gmail.com>
2025-01-19 23:16:13 +0500

    new hannan

--- script1.js
+++ script1.js
@@ -6,3 +6,4 @@ console.log(`The sum of ${a} and ${b} is ${sum}`);
 const difference = a - b;
 
 console.log(`The difference between ${a} and ${b} is ${difference}`);
+console.log("testing script1.js");f
