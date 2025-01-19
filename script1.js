fb28fe11b7f6b1c9d56fbed764d9260c22863b54
hennan1 <hannanbutt0267@gmail.com>
2025-01-19 23:15:42 +0500

    hannan

--- script1.js
+++ script1.js
@@ -3,3 +3,6 @@ const b = 10;
 const sum = a + b;
 
 console.log(`The sum of ${a} and ${b} is ${sum}`);
+const difference = a - b;
+
+console.log(`The difference between ${a} and ${b} is ${difference}`);
