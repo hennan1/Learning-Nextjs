4e0fc570b064a37b1ecd5dfbe4fa7a9e1fc00f3c
hennan1 <hannanbutt0267@gmail.com>
2025-01-21 18:27:40 +0500

    made some changes to the imports

--- script1.js
+++ script1.js
@@ -32,3 +32,8 @@ console.log(`The quotient of ${a} and ${b} is ${quotient}`);
 // Log a message indicating the end of calculations
 console.log("End of calculations.");
 console.log("what are you doing ")
+
+
+
+// hannan
+console.log("hannan");
